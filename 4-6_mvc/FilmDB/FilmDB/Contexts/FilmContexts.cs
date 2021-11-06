51d00eceee7c950da4b67351b50f73388485779c
Pindor Szymon <s6035sp@ms.wwsi.edu.pl>
2021-11-06 11:22:29 +0100

    add connection to DB

---
+++ 4-6_mvc/FilmDB/FilmDB/Contexts/FilmContexts.cs
@@ -0,0 +1,21 @@
+﻿using FilmDB.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmDB.Contexts
+{
+    public class FilmContexts : DbContext
+    {
+        public DbSet<FilmModel> Films { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextoptions)
+        {
+            var cs = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=FilmDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            dbContextoptions.UseSqlServer(cs);
+        }
+    }
+}
